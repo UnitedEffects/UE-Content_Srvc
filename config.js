a5e4cab1c38ac09e01a108231cb010c8055064fb
Bo Motlagh <bo.motlagh@gmail.com>
2017-03-15 22:09:23 -0400

    replica set

--- config.js
+++ config.js
@@ -6,7 +6,8 @@ var config = {
     defaultMongo: (process.env.MONGO) ? process.env.MONGO : 'mongodb://localhost:27017/ue-content',
     swaggerDomain: (process.env.SWAG_DOM) ? process.env.SWAG_DOM : 'localhost:3010',
     gatewayDomain: (process.env.GATEWAY) ? process.env.GATEWAY : 'localhost:6000',
-    userApiServer: (process.env.USERAUTH) ? process.env.USERAUTH : 'http://localhost:4000'
+    userApiServer: (process.env.USERAUTH) ? process.env.USERAUTH : 'http://localhost:4000',
+    replica: process.env.REPLICA
 };
 
 module.exports = config;
